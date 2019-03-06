c139b530c12ef15c09ab11892baf9b2c173d4648
xueflower <39401916+xueflower@users.noreply.github.com>
2019-03-06 13:50:03 +0800

    11
    11

---
+++ digital/digital/js/common.js
@@ -0,0 +1,14 @@
+// 移动端响应式匹配 px转rem=20
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+	var deviceWidth = document.documentElement.clientWidth;
+	var deviceHeight = document.documentElement.clientHeight;
+			document.documentElement.style.fontSize = deviceWidth / 96 + 'px';
+	   }, false);
+	   
+	window.onresize = function(){
+		var deviceWidth = document.documentElement.clientWidth;
+		var deviceHeight = document.documentElement.clientHeight;
+			document.documentElement.style.fontSize = deviceWidth / 96+ 'px';
+	};
+})();
